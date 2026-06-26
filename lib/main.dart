41a8e17587094f9fecfedb61e8b1e19bd1392def
dharmikparmar576-sketch <dharmikparmar576@gmail.com>
2026-06-26 18:25:13 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catlog/pages/home_page.dart';
+import 'package:flutter_catlog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.deepPurple,
           foregroundColor: Colors.white, // Text color changes (Catalog App)
         ),
       ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
